23de7c38ea1fa4562f180a0ba73d71aa40ef5371
Piotr Mitros <pmitros@mitx.mit.edu>
2014-06-24 21:33:13 -0400

    Continuing transition to templates

--- profile/static/js/src/profile.js
+++ profile/static/js/src/profile.js
@@ -2,16 +2,21 @@
 var profile_data = { 
     'class' : 'ProfileMainBlock', 
     'children' : [
-	{ 'class' : 'ProfileStaticText', 
-	  'source' : 'profile_overview'},
-	{ 'class' : 'ProfileStaticText', 
-	  'source' : 'contact_information'},
-	{ 'class' : 'ProfileStaticText', 
-	  'source' : 'demographics'},
-	{ 'class' : 'ProfileStaticText', 
-	  'source' : 'photo'},
-	{ 'class' : 'ProfileStaticText', 
-	  'source' : 'background'}
+//	{'class' : 'ProfileColumn', 
+//	 'children' : [
+	     { 'class' : 'ProfileStaticText', 
+	       'source' : 'profile_overview'},
+	     { 'class' : 'ProfileStaticText', 
+	       'source' : 'contact_information'},
+	     { 'class' : 'ProfileStaticText', 
+	       'source' : 'demographics'},
+//	 ]},
+//	{'class' : 'ProfileColumn', 
+//	 'children' : [
+	     { 'class' : 'ProfileStaticText', 
+	       'source' : 'photo'},
+	     { 'class' : 'ProfileStaticText', 
+	       'source' : 'background'}//]}
 	/*    { 'class' : 'profile_block', 
 	      'title' : 'Contact Information',
 	      'contents' : {}},
@@ -47,7 +52,7 @@ function ProfileXBlock(runtime, element) {
 	$.fn.ProfileBlock = function( options ) {
 	    options["id"] = "profile_block"; 
 	    this[options['class']](options);
-	}	
+	}
 	$.fn.ProfileMainBlock = function( options ) {
 	    var id = options.id;
 	    for(i=0; i<options.children.length; i++){
@@ -60,6 +65,20 @@ function ProfileXBlock(runtime, element) {
 		$("#"+options.children[i].id).ProfileBlock(options.children[i]);
 	    }
 	}
+
+	$.fn.ProfileColumn = function( options ) {
+	    var id = options.id;
+	    for(i=0; i<options.children.length; i++){
+		var new_id = id+'_'+i;
+		$.extend(options.children[i], {'id': new_id});
+		options.children[i]["render"]='<div id="'+new_id+'"/>';
+	    }	
+	    this.html(Mustache.render($("#column_block").html(),options));
+	    for(i=0; i<options.children.length; i++){
+		$("#"+options.children[i].id).ProfileBlock(options.children[i]);
+	    }
+	}
+
 	$.fn.ProfileStaticText = function( options ) {
 	    console.log($("#"+options.source).html());
 	    this.html(Mustache.render($("#"+options.source).html(),options));
