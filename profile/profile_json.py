8ac64ddb36086019427ecccafd24f8b01a3561fb
Piotr Mitros <pmitros@mitx.mit.edu>
2014-09-05 11:55:08 -0400

    Profile for edX-internal course

--- profile/profile_json.py
+++ profile/profile_json.py
@@ -1,4 +1,136 @@
-profile_config = {
+edx_profile_config = {
+    "class" : "ProfileMainBlock", 
+    "children" : [
+	{"class" : "ProfileColumn", 
+	 "children" : [
+	     { "class" : "ProfileStaticText", 
+	       "source" : "profile_overview"},
+	     { "class" : "ProfileForm", 
+	       "title" : "Contact Information", 
+	       "children" : [
+		   {"class"    : "ProfileOneLiner", 
+		    "question" : "Name", 
+		    "field" : "edx_name",
+		    "placeholder" : "Your name" },
+		   {"class" : "ProfileContactInfo", 
+		    "children": [
+			{"label":"Telephone", 
+			 "field":"edx_phone",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "1(617)234-5678",
+			 "icon" : "phone"
+			},
+			{"label":"E-mail:", 
+			 "field":"edx_email",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "jsmith@edx.org",
+			 "icon" : "email"
+			},
+			{"label":"Web site: ", 
+			 "field":"edx_website",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "http://www.edx.org/",
+			 "icon" : "pages"
+			},
+			{"label":"Skype username", 
+			 "field":"edx_skype",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "skype"
+			},
+			{"label":"http://facebook.com/", 
+			 "field":"edx_facebook",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "facebook",
+			 "help" : "For help reserving a Facebook URL, see https://www.facebook.com/help/200712339971750#How-do-I-customize-my-timeline-or-Page-address?-Where-can-I-claim-a-username?"
+			},
+			{"label":"http://plus.google.com/", 
+			 "field":"edx_googleplus",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "google-plus"
+			},
+			{"label":"http://github.com/", 
+			 "field":"edx_github",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "github"
+			},
+			{"label":"http://linkedin.com/in/", 
+			 "field":"edx_linkedin",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "linkedin", 
+			 "help": "For help reserving a LinkedIn URL, see http://help.linkedin.com/app/answers/detail/a_id/87"
+			},
+			{"label":"http://twitter.com/", 
+			 "field":"edx_twitter",
+			 "class":"ProfileContactBox", 
+			 "placeholder" : "",
+			 "icon" : "twitter"
+			}
+		   ]}
+	       ]},
+	     { "class" : "ProfileForm", 
+	       "title" : "Demographics", 
+	       "children" : [
+		   {"class"    : "ProfileOneLiner", 
+		    "question" : "Where do you live?",
+		    "field" : "location", 
+		    "placeholder" : "East Cambridge" },
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "What languages are you fluent in?", 
+		    "placeholder" : "English", 
+		    "field" : "edx_languages",
+		    "rows":2},
+		   {"class"    : "ProfileDropDown", 
+		    "field"    : "edx_age", 
+		    "question" : "How old are you?", 
+		    "choices" : [{"item": "Prefer not to say"}, {"item":"Under 13"}, {"item":"14-17"}, {"item":"18-24"}, {"item":"25-35"}, {"item":"35-50"}, {"item":"Over 50"}]}
+	       ]}
+	 ]},
+	{"class" : "ProfileColumn", 
+	 "children" : [
+	     { "class" : "ProfilePhoto", 
+	       "source" : "photo"},
+	     { "class" : "ProfileForm", 
+	       "title" : "Background", 
+	       "children" : [
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "What part of the organization are you in?",
+		    "field" : "org-position", 
+		    "placeholder" : "Engineering/Teaching and Learning" },
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "What is your background in education? Have you taught? Are you familiar with any areas of education research? Ed-tech? Psychology? Etc?", 
+		    "placeholder" : "I'm a developer; not much education per-se, but I do have a strong background in gamification", 
+		    "field" : "cphys.edbackground",
+		    "rows":4},
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "What is your background in technology? Front-end? Back-end? Product? UX? How familiar are you with the edX platform?", 
+		    "field" : "cphys.techbackground",
+		    "placeholder" : "I am a back-end developer, but I do enjoy graphic design. Still need to learn CSS, though.", 
+		    "rows":3},
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "Tell us a bit about yourself. Write a brief bio.", 
+		    "placeholder" : "Biographical Information", 
+		    "field" : "cphys.bio",
+		    "rows":4},
+		   {"class"    : "ProfileTextArea", 
+		    "question" : "Finally, what drew you to edX? Why are you here?", 
+		    "placeholder" : "I want to modernize education in Walachia", 
+		    "field" : "motiviation",
+		    "rows":4}
+	       ]}
+	 ]}
+    ]}
+
+
+
+
+
+
+physics_profile_config = {
     "class" : "ProfileMainBlock", 
     "children" : [
 	{"class" : "ProfileColumn", 
@@ -216,3 +348,5 @@ peer_profile_config = {
                    ]},
              ]}
         ]}
+
+profile_config = edx_profile_config
